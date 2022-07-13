61fc594ee98239ad86c2e42350c73732f3ce2352
Rebecca Wang <73043927+rbccawang@users.noreply.github.com>
2022-07-13 15:40:05 -0700

    Update

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-'''
+''' 
 Rotate Servo Back-and-Forth
 Rebecca Wang
 2022 July 13
@@ -6,16 +6,21 @@ Rebecca Wang
 
 import adafruit_motor
 import board
-import time
+import time 
 import pwmio
 
-# pwm variable
+# pwm variable - pulse width modulation
 # dutycycle tells you if it's off or on (2^16; halfway is 2^15)
-pwm = pwmio.PWMOut(board.A1, duty_cycle = 2**15, frequency = 50)
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2**15, frequency=50)
 
 # servo variable
 srv = adafruit_motor.servo.Servo(pwm)
 
-# main loop
-while True:
-    aaa
+while True: 
+    for ang in range(0, 185, 5):
+        srv.angle = ang
+        time.sleep(0.1)  # 0.1 seconds
+        
+        for ang in range(180, 0, -5):
+            srv.angle = ang
+            time.sleep(0.1)  # 0.1 seconds
